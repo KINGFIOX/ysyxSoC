dee8b9355faf61e3aed87677c382fc48e9d16700
wangfiox <wangfiox@gmail.com>
2026-03-22 00:37:09 +0800

    gshare

--- src/scala/core/frontend/BHT.scala
+++ src/scala/core/frontend/BHT.scala
@@ -5,35 +5,30 @@ import chisel3.util._
 
 import ysyx.core.common._
 
-// branch history table
-class BHTUpdate extends NPCBundle {
-  val br_flag = Bool() // jmp or not
+// gshare branch predictor
+class BHTUpdate(ghrBits: Int) extends NPCBundle {
+  val br_flag = Bool()
   val pc = UInt()
+  val ghr = UInt(ghrBits.W) // ght snapshot at prediction time
 }
 
-class BHTEntry(tagBits: Int, countBits: Int) extends NPCBundle {
-  val count = UInt(countBits.W) // jmp or not
-  val occupied = Bool()
-  val tag = UInt(tagBits.W)
-}
-
-class BHTReadPort extends NPCBundle {
+class BHTReadPort(ghrBits: Int) extends NPCBundle {
   val pc = UInt(addrBits.W)
   val br_flag = Input(Bool())
-  val hit = Input(Bool())
+  val ghr = Input(UInt(ghrBits.W))
 }
 
 class BHT(entries: Int = 4096, countBits: Int = 2) extends NPCModule {
   require(isPow2(entries))
   private val entriesBits = log2Ceil(entries)
-  private val tagBits = addrBits - entriesBits - 2 /*pc would be aligned to 2*/
 
   val io = IO(new Bundle {
-    val update = Flipped(Valid(new BHTUpdate))
-    val lookup = Flipped(new BHTReadPort)
+    val update = Flipped(Valid(new BHTUpdate(entriesBits)))
+    val lookup = Flipped(new BHTReadPort(entriesBits))
   })
 
-  val lht = Reg( Vec(entries, new BHTEntry(tagBits, countBits))) // local history table
+  val ght = RegInit(0.U(entriesBits.W)) // global history table
+  val pht = Reg(Vec(entries, UInt(countBits.W))) // pattern history table
 
   private def extractIdx(pc: UInt): UInt = {
     val lsb: Int = 2
@@ -41,32 +36,23 @@ class BHT(entries: Int = 4096, countBits: Int = 2) extends NPCModule {
     pc(msb, lsb)
   }
 
-  private def extractTag(pc: UInt): UInt = {
-    val lsb: Int = 2 + entriesBits
-    val msb: Int = 2 + entriesBits + tagBits - 1
-    pc(msb, lsb)
-  }
-
   // lookup
-  val idx = extractIdx(io.lookup.pc)
-  val tag = extractTag(io.lookup.pc)
-  io.lookup.br_flag := lht(idx).count( countBits - 1) // msb to indicate jmp or not
-  io.lookup.hit := lht(idx).occupied && (lht(idx).tag === tag)
+  val idx = extractIdx(io.lookup.pc) ^ ght
+  io.lookup.br_flag := pht(idx)(countBits - 1)
+  io.lookup.ghr := ght
 
   // update
   when(io.update.valid) {
     val u = io.update.bits
-    val idx = extractIdx(u.pc)
-    val tag = extractTag(u.pc)
-    val c = lht(idx).count
+    val idx = extractIdx(u.pc) ^ u.ghr
+    val c = pht(idx)
     val max = ((1 << countBits) - 1).U
     when(u.br_flag) {
-      lht(idx).count := Mux(c === max, max, c + 1.U)
+      pht(idx) := Mux(c === max, max, c + 1.U)
     }.otherwise {
-      lht(idx).count := Mux(c === 0.U, 0.U, c - 1.U)
+      pht(idx) := Mux(c === 0.U, 0.U, c - 1.U)
     }
-    lht(idx).tag := tag
-    lht(idx).occupied := true.B
+    ght := Cat(ght(entriesBits - 2, 0), u.br_flag)
   }
 
 }
